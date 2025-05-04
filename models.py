a105eb96f778eee15faec03e2cf48ce83c6613cd
Max Bennett <22974298@student.uwa.edu.au>
2025-05-05 02:26:01 +0800

    Login in features with database. Still needs further work on sign up capabilities.

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Goal(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    wam = db.Column(db.Float, nullable=False)
-    gpa = db.Column(db.Float, nullable=False)
