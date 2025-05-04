67a9a8210d569f798a259963ee29ac66ffc9ddb2
Brian Keng Seong Chan <163382886+BrianC-04@users.noreply.github.com>
2025-05-04 16:30:39 +0800

    Update app.py
    Added new logic to route results to temporary results page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, json, render_template
 
 app = Flask(__name__)
 
@@ -14,5 +14,42 @@ if __name__ == '__main__':
 
 @app.route('/')
 def home():
-    return render_template('calculator.html')  # or whatever your homepage is
+    return render_template('test.html')  # or whatever your homepage is
 
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    raw_data = request.form['data']
+    data = json.loads(raw_data)
+
+    unit_scores = []
+    for unit in data['Previous Units']:
+        unit_scores.append(unit[2])
+
+    for unit in data:
+        if unit != 'Previous Units':
+            score = 0
+            for assessment in data[unit]["assessments"]:
+                # weighting = [1], max = [2], marks = [3]
+                score += assessment[1] * (assessment[3] / assessment[2])
+            unit_scores.append(score)
+
+    unit_count = len(unit_scores)
+    total_scores = sum(unit_scores)
+    total_gpa = 0
+    for score in unit_scores:
+        if score >= 80:
+            total_gpa += 7.0
+        elif score >= 70:
+            total_gpa += 6.0
+        elif score >= 60:
+            total_gpa += 5.0
+        elif score >= 50:
+            total_gpa += 4.0
+        else:
+            total_gpa += 0.0
+
+    gpa = total_gpa / unit_count
+    wam = total_scores / unit_count
+
+    return render_template('resulttest.html', gpa=gpa, wam=wam)
