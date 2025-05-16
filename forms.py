e050acc40f6a06daf5f80b8a866a0a78a4aeb422
Brian Keng Seong Chan <163382886+BrianC-04@users.noreply.github.com>
2025-05-16 13:42:00 +0800

    Update forms.py
    Added new form types for calculator and updated import items to include required classes/methods.

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from wtforms import EmailField, PasswordField, SubmitField, StringField, SelectField, IntegerField, FieldList, FormField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length, NumberRange
 from models import db, User
 import sqlalchemy as sa
 
@@ -19,3 +19,36 @@ class Sign_upForm(FlaskForm):
         user = db.session.scalar(sa.select(User).where(User.email == email.data))
         if user is not None:
             raise ValidationError('Email already registered.')
+
+class AssessmentForm(FlaskForm):
+    class Meta:
+        csrf = False
+    atype = StringField('Assessment Type')
+    weight = IntegerField('Weight (%)')
+    max_mark = IntegerField('Available Marks')
+    student_mark = IntegerField('Your Marks')
+
+class UnitForm(FlaskForm):
+    class Meta:
+        csrf = False
+    unit = StringField('Unit Code')
+    semester = StringField('Semester')
+    year = StringField('Year')
+    assessments = FieldList(FormField(AssessmentForm), min_entries=1)
+
+class PreviousUnitForm(FlaskForm):
+    class Meta:
+        csrf = False
+    unit = StringField('Unit Code')
+    semester = StringField('Semester')
+    year = StringField('Year')
+    mark = IntegerField('Your Mark (%)')
+
+class PreviousUnitTabForm(FlaskForm):
+    class Meta:
+        csrf = False
+    previous_units = FieldList(FormField(PreviousUnitForm), min_entries=1)
+
+class CalcForm(FlaskForm):
+    previous_units_tab = FieldList(FormField(PreviousUnitTabForm), min_entries=1, max_entries=1)
+    units = FieldList(FormField(UnitForm))
